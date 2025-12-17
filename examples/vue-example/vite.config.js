3d55e30ddf4f15705491954c8741928f3001232a
chocozhang <happyelement.danny@gmail.com>
2025-12-17 11:29:30 +0800

    chore: configure main branch to use online three-model-render package; dev branch uses local tool functions for testing

--- examples/vue-example/vite.config.js
+++ examples/vue-example/vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import path from 'path'
-
 export default defineConfig({
     plugins: [vue()],
-    resolve: {
-        alias: {
-            '@chocozhang/three-model-render/setup': path.resolve(__dirname, '../../src/setup/index.ts'),
-            '@chocozhang/three-model-render/loader': path.resolve(__dirname, '../../src/loader/index.ts'),
-            '@chocozhang/three-model-render/core': path.resolve(__dirname, '../../src/core/index.ts'),
-            '@chocozhang/three-model-render/camera': path.resolve(__dirname, '../../src/camera/index.ts'),
-            '@chocozhang/three-model-render/effect': path.resolve(__dirname, '../../src/effect/index.ts'),
-            '@chocozhang/three-model-render/interaction': path.resolve(__dirname, '../../src/interaction/index.ts'),
-            '@chocozhang/three-model-render/ui': path.resolve(__dirname, '../../src/ui/index.ts'),
-            '@chocozhang/three-model-render': path.resolve(__dirname, '../../src/index.ts'),
-            'three': path.resolve(__dirname, 'node_modules/three')
-        }
-    },
     optimizeDeps: {
         include: ['three']
     }
